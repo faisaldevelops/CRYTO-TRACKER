fb2ad6d36ffc5e5e750a3e2dc066fd6b5a391810
FAISAL MAJEED <faisalmajeed9878@gmail.com>
2022-03-09 15:42:32 +0530

    return problem for array.map

--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -178,7 +178,7 @@ export default function UserSidebar() {
                            );
                            else{
                               return(
-                                 <div></div>
+                                 null
                               );
                            }
                         })
